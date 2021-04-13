5a913488eb5ced601f245cf826f15b07d80f0d6c
Andrew <wing_andrew@hotmail.com>
2021-04-12 21:37:42 -0400

    Hiding timer and page elements until start button clicked

--- Assets/js/quiz.js
+++ Assets/js/quiz.js
@@ -1,3 +1,9 @@
+//universal variables
+var highScore;
+var questionCount;
+// Event Listeners
+
+
 
 // Jump to next input box when first initial is added.
 $(".inputs").keyup(function () {
@@ -5,3 +11,43 @@ $(".inputs").keyup(function () {
       $(this).next('.inputs').focus();
     }
 });
+
+// Timer Function ------------------------------------------------------------------
+
+// variables to pass elements from HTML to Javascript
+var startTimer = document.getElementById("startTimer");
+var timerBlock = document.getElementById('timerBlock');
+var elem = document.getElementById('counter');
+
+// event listener
+startTimer.addEventListener("click", timer);
+
+var timeLeft = 30; // starting time in seconds
+    
+    
+function timer() {  
+  
+  var timerInterval = setInterval(function(){
+    
+    document.getElementById('timerBlock').style.display = "block"; //display timer
+
+    //when there is less than 10 seconds remaining change text to red
+    if (timeLeft <= 9){
+      document.getElementById("timerBlock").style.color = "red";
+      document.getElementById("counter").innerHTML= "00:0" + timeLeft;
+    } else {
+      document.getElementById("counter").innerHTML= "00:" + timeLeft;
+    }
+
+    //if time runs out or the questions are all answered end the game and stop the timer
+    if (timeLeft === 0 || questionCount === 4) {
+      clearInterval(timerInterval);
+      document.getElementById("counter").innerHTML = "DONE";
+      endGame();
+      return
+  } 
+  timeLeft--;
+  }, 1000);
+
+};
+
